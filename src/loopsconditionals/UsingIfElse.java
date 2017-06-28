a7db09886a38ab10931e63f56218dac778910214
renukamatkar <renuka.matkar92@gmail.com>
2017-06-28 19:53:49 -0400

    v1

---
+++ src/loopsconditionals/UsingIfElse.java
@@ -0,0 +1,13 @@
+package loopsconditionals;
+public class UsingIfElse {
+
+	   public static void main(String args[]) {
+	      int x = 30;
+
+	      if( x < 20 ) {
+	         System.out.print("This is if statement");
+	      }else {
+	         System.out.print("This is else statement");
+	      }
+	   }
+	}
---
+++ src/loopsconditionals/UsingWhile.java
@@ -0,0 +1,13 @@
+package loopsconditionals;
+
+public class UsingWhile {
+
+	   public static void main(String args[]) {
+	      int x = 10;
+
+	      while( x < 20 ) {
+	         System.out.print( x );
+	         x++;
+	      }
+	   }
+	}
