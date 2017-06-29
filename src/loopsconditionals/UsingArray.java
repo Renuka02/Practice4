81e93f47927461b6959fbc336bbd52d35db60cbe
renukamatkar <renuka.matkar92@gmail.com>
2017-06-28 20:06:23 -0400

    v2

---
+++ src/loopsconditionals/UsingArray.java
@@ -0,0 +1,12 @@
+package loopsconditionals;
+
+public class UsingArray {
+
+	   public static void main(String[] args) {
+	      double[] myList = {1.9, 2.9, 3.4, 3.5};
+
+	      for (double element: myList) {
+	         System.out.println(element);
+	      }
+	   }
+	}
